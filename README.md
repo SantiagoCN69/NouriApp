40d9d4cb8d1fe16a2ae96dc75e0c61a06681b29a
SANTIAGOCN69 <SANTIAGOCN08@GMAIL.COM>
2026-05-06 10:25:22 -0500

    First commit, nouri proyect

---
+++ README.md
@@ -0,0 +1,11 @@
+
+  # Crear landing page moderna
+
+  This is a code bundle for Crear landing page moderna. The original project is available at https://www.figma.com/design/WLD714bzq2VS91nGJgAIg1/Crear-landing-page-moderna.
+
+  ## Running the code
+
+  Run `npm i` to install the dependencies.
+
+  Run `npm run dev` to start the development server.
+  
---
+++ src/imports/Negro/Negro.tsx
@@ -0,0 +1,17 @@
+import svgPaths from "./svg-iot85za4ei";
+
+export default function Negro() {
+  return (
+    <div className="relative size-full" data-name="Negro">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 3844 1263">
+        <g id="Negro">
+          <path d={svgPaths.p2fa85180} fill="var(--fill-0, black)" id="Vector" style={{ fill: "black", fillOpacity: "1" }} />
+          <path d={svgPaths.p1e74cb00} fill="var(--fill-0, black)" id="Vector_2" style={{ fill: "black", fillOpacity: "1" }} />
+          <path d={svgPaths.pb4d1500} fill="var(--fill-0, black)" id="Vector_3" style={{ fill: "black", fillOpacity: "1" }} />
+          <path d={svgPaths.p13535680} fill="var(--fill-0, black)" id="Vector_4" style={{ fill: "black", fillOpacity: "1" }} />
+          <path d={svgPaths.p381f2e80} fill="var(--fill-0, black)" id="Vector_5" style={{ fill: "black", fillOpacity: "1" }} />
+        </g>
+      </svg>
+    </div>
+  );
+}
